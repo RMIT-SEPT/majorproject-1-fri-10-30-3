2c802642fdffa919a5e4fc7fd1d19a83abd0d13c
[Man Hou s3795574] <s3795574@student.rmit.edu.au>
2020-08-09 12:43:30 +1000

    Create an empty project for backend. -add four folders, they are model, Repositories, services and web; -add H2 configuration in application.properties. Url:http://localhost:8080/h2-console NOTE: create a new branch when implement features.

---
+++ BackEnd/src/main/java/com/rmit/sept/fri_10_30_3/majorproject/MajorprojectApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.fri_10_30_3.majorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajorprojectApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/rmit/sept/fri_10_30_3/majorproject/MajorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.fri_10_30_3.majorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
