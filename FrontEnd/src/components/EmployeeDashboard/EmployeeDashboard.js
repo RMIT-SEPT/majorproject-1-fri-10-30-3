00c807661f194c33b42c2431375f4a073f441a09
s3734613 <s3734613@student.rmit.edu.au>
2020-10-14 20:20:31 +1100

    Employee Dashboard

--- FrontEnd/src/components/EmployeeDashboard/EmployeeDashboard.js
+++ FrontEnd/src/components/EmployeeDashboard/EmployeeDashboard.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react'
-
-
+import React, { Component } from 'react';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import "./EmployeeDashboard.css"
 import { Link } from "react-router-dom";
 
+
 class EmployeeDashboard extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       fname: '',
       lname: '',
       userName: '',
       password: '',
-      id: 3,
-      records: []
+      id: props.match.params.id,
+      schedule: [],
+      skills: []
     }
   }
 
   componentDidMount() {
-    this.updateCustomerDetails()
-    this.updateBookingRecords()
+    this.updateEmployeeDetails()
+    this.updateScheduleRecords()
+    this.updateSkills()
   }
 
-  updateCustomerDetails() {
+  updateEmployeeDetails() {
     fetch(`http://localhost:8080/api/employee/${this.state.id}`)
       .then(res => res.json())
       .then(res => {
@@ -34,32 +35,80 @@ class EmployeeDashboard extends Component {
       })
   }
 
-  updateBookingRecords() {
-    fetch(`http://localhost:8080/api/enrollment`)
+  updateScheduleRecords() {
+    fetch(`http://localhost:8080/api/schedule`)
       .then(res => res.json())
       .then(res => {
         const filtered = res.filter(b => b.employee.id === this.state.id)
-        const parsed = this.parseBooking(filtered)
-
+        const parsed = this.parseSchedule(filtered)
+        
         this.setState({
-          records: parsed
+          schedule: parsed
         })
       })
   }
 
-  parseBooking(data) {
+  updateSkills(){
+    fetch(`http://localhost:8080/api/skills`)
+      .then(res => res.json())
+      .then(async res => {
+        const skillId = await fetch(`http://localhost:8080/api/schedule`)
+          .then(res => res.json())
+          .then(res => Array.isArray(res) ? res : [res])
+          .then(res => res.map(s => s.skills.skillId))
+          .then(res =>  res.filter((value, index, self) => {
+            return self.indexOf(value) === index;
+          }))
+          console.log(skillId)
+        const filtered = res.filter(b => skillId.find(s => s === b.skillId))
+        const parsed = this.parseSkill(filtered)
+        
+        this.setState({
+          skills: parsed
+        })
+      })
+  }
+
+  parseSchedule(data) {
     return data.map((b, i) => {
       return (
         <tr 
           className="customer-dashboard-record" 
-          onClick={() => { this.props.history.push(`/read/booking/${b.enrollmentId}`) }} 
+          onClick={() => { this.props.history.push(`/read/schedule/${b.scheduleId}`) }} 
           key={i}
         >
-          <td>{b.employee_schedule.availability}</td>
-          <td>{this.convertTime(b.employee_schedule.startingHour)}</td>
-          <td>{b.enrollmentId}</td>
-          <td>{b.employee_schedule.employee.fname + " " + b.employee_schedule.employee.lname}</td>
-          <td>{b.employee_schedule.skills.title}</td>
+          <td>{b.scheduleId}</td>
+          <td>{b.employee.id}</td>
+          <td>{b.skills.skillid}</td>
+          <td>{b.capacity}</td>
+          <td>{b.price}</td>
+          <td>{b.availability}</td>
+          <td>{b.startingHour}</td>
+          <td>{b.length}</td>
+          <td>{b.createdAt}</td>
+          <td>{b.updatedAt}</td>
+        </tr>
+      )
+    })
+  }
+
+  parseSkill(data){
+    return data.map((b, i) => {
+      return (
+        <tr 
+          className="customer-dashboard-record" 
+          onClick={() => { this.props.history.push(`/read/skills/${b.skillId}`) }} 
+          key={i}
+        >
+          <td>{b.skillId}</td>
+          <td>{b.skillsName}</td>
+          <td>{b.title}</td>
+          <td>{b.imageSrc}</td>
+          <td>{b.description}</td>
+          <td>{b.length}</td>
+          <td>{b.cost}</td>
+          <td>{b.createdAt}</td>
+          <td>{b.updatedAt}</td>
         </tr>
       )
     })
@@ -109,13 +158,37 @@ class EmployeeDashboard extends Component {
             <table className="customer-dashboard-table">
               <tbody>
                 <tr>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>ID</th>
-                  <th>Employee</th>
-                  <th>Skill</th>
+                  <th>Schedule ID</th>
+                  <th>Empoyee ID</th>
+                  <th>Skill ID</th>
+                  <th>Capacity</th>
+                  <th>Price</th>
+                  <th>Availability</th>
+                  <th>Starting Hour</th>
+                  <th>Length</th>
+                  <th>Created At</th>
+                  <th>Updated At</th>
                 </tr>
-                {this.state.records}
+                {this.state.schedule}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="customer-dashboard-table-container">
+            <table className="customer-dashboard-table">
+              <tbody>
+                <tr>
+                  <th>Skill ID</th>
+                  <th>Skill Name</th>
+                  <th>Title</th>
+                  <th>Image Src</th>
+                  <th>Description</th>
+                  <th>Length</th>
+                  <th>Cost</th>
+                  <th>Created At</th>
+                  <th>Updated At</th>
+                </tr>
+                {this.state.skills}
               </tbody>
             </table>
           </div>
